ad356750432b2a8657e38a8e944d2e61e4044a08
Pravat <pravatpatrap@outlook.com>
2018-11-25 13:10:18 +0530

    Test case added

---
+++ src/test/java/com/parser/logs/LogParser/ArgumentExceptionTest.java
@@ -0,0 +1,11 @@
+package com.parser.logs.LogParser;
+
+import junit.framework.TestCase;
+
+public class ArgumentExceptionTest extends TestCase {
+
+	public void testArgumentException() {
+		fail("Not yet implemented");
+	}
+
+}

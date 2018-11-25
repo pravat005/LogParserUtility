0961c4e666abaa6b0426719d85f52276554b3911
Pravat <pravatpatrap@outlook.com>
2018-11-25 13:13:19 +0530

    test class added

---
+++ src/test/java/com/parser/logs/LogParser/CommandLineParserTest.java
@@ -0,0 +1,11 @@
+package com.parser.logs.LogParser;
+
+import junit.framework.TestCase;
+
+public class CommandLineParserTest extends TestCase {
+
+	public void testParseCommandLineArgs() {
+		fail("Not yet implemented");
+	}
+
+}
